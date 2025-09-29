056db3d2a1b1c7f9512ebc53bb26a2a027aa43a0
Bramaramba G <gurijalabramaramba523@gmail.com>
2025-09-29 22:40:27 +0530

    Add files via upload

---
+++ TerinaryWithString.java
@@ -0,0 +1,12 @@
+package Sampleone;
+
+public class TerinaryWithString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int age=45;
+String result=(age>=18)? "adult" : "Minor";
+System.out.println(result);
+	}
+
+}
